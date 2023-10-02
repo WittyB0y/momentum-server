fb95e0b6b42dd7a336c9dd88bdc362e2ee18b047
a.babrounik <a.babrounik@astondev.ru>
2023-10-03 00:35:53 +0300

    new(.flake8): added flake8

--- momentumServer/settings.py
+++ momentumServer/settings.py
@@ -47,8 +47,7 @@ ROOT_URLCONF = 'momentumServer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
