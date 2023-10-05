72a4e1cc0a92906823fb5ce909558158d5b8636a
WittyB0y <99145733+WittyB0y@users.noreply.github.com>
2023-10-06 02:00:18 +0300

    MYI-18: added create endpoints for crud friends (#12)
    Co-authored-by: a.babrounik <a.babrounik@astondev.ru>

--- friend/urls.py
+++ friend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from friend.views import RequestToAddFriend, GetAllUserFriend
+from friend.views import RequestToAddFriend, GetAllUserFriend, AcceptUserRequest, DestroyFriend
 
 urlpatterns = [
-    path('add/', RequestToAddFriend.as_view(), name='add_friend'),
+    path('follow/', RequestToAddFriend.as_view(), name='add_friend'),
+    path('follow/accept/', AcceptUserRequest.as_view(), name='accept_friend_request'),
+    path('delete/', DestroyFriend.as_view(), name='destroy_friend'),
     path('my/', GetAllUserFriend.as_view(), name='my_friends'),
 ]
